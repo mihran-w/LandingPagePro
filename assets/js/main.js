769977c9eb9b96a2aef028caddbe9a13c9ffd571
mihran-w <mihranw@gmail.com>
2022-02-25 22:08:48 +0330

    Add First Theme Project

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// Header NavBar Scrolled
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var y = window.scrollY;
+    if (y >= 100) {
+      $("#navbar").addClass("header-scrolled");
+    } else {
+      $("#navbar").removeClass("header-scrolled");
+    }
+  });
+});
+// Header NavBar Scrolled End
+
+// PreLoader Start
+$(window).on("load", function () {
+  $(".preloader").delay(1000).fadeOut("slow");
+  $(".pre-div").delay(1000).fadeOut("slow");
+});
+// Preloader End
+
+// Pure Counters Init
+$(document).ready(function () {
+  new new PureCounter()();
+});
